c1300598468f4db4c8bca64cf0248f4c481eb2cc
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-03 16:18:18 +0200

    react+php

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import  {React, useState, useEffect } from 'react';
 
 function App() {
+
+  const [t1, setT1] = useState('');
+
+  function clickHandler() {
+    fetch('http://test1.ua', {
+      method: 'POST',
+      header: {
+        'Content-type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({action: 1})
+    })
+      .then(response => response.text())
+      .then(response => {
+      console.log(response)
+      setT1(response)
+     })
+  }
   return (
-    <div>
-  
+    <div className="container">
+      <h1>Hello World!</h1>
+      <button onClick={clickHandler}>Go</button>
+      <div>
+        {t1}
+      </div>
     </div>
   );
 }
